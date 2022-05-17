bf63ce0440e7016f443cd397cd4b3043d2e0cd55
Brandon Seymour <bseymour122@yahoo.ca>
2022-05-17 17:31:20 -0400

    Add comments

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,6 @@
-let myLibrary = [];
+let myLibrary = [];// Holds book objects
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read) {// Constructor to make book objects
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -9,12 +9,15 @@ function Book(title, author, pages, read) {
 Book.prototype.addBookToLibrary = function () {
     myLibrary.push(this);
 }
+/* Displays book objects using DOM manipulation. book.hasOwnProperty(key) was used so 
+that the addBookToLibrary method is excluded when looping through book object. This is becuase
+each book object inherits the addBookToLibrary method from Book prototype property*/
 function displayBook() {
     const table = document.querySelector('tbody');
     myLibrary.forEach(book => {
         let tableRow = document.createElement('tr');
         for (const key in book) {
-            if (book.hasOwnProperty(key)) {;
+            if (book.hasOwnProperty(key)) {
                 const element = book[key];
                 let tableData = document.createElement('td');
                 tableData.textContent = element;
