41cb3c58cb2d73a8ad34ae93a98de700dfad0471
Brandon Seymour <bseymour122@yahoo.ca>
2022-05-17 16:08:14 -0400

    Add displayBook function

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,3 +9,18 @@ function Book(title, author, pages, read) {
 Book.prototype.addBookToLibrary = function () {
     myLibrary.push(this);
 }
+function displayBook() {
+    const table = document.querySelector('tbody');
+    myLibrary.forEach(book => {
+        let tableRow = document.createElement('tr');
+        for (const key in book) {
+            if (Object.hasOwnProperty.call(book, key)) {
+                const element = book[key];
+                let tableData = document.createElement('td');
+                tableData.textContent = element;
+                tableRow.appendChild(tableData);
+            }
+            table.appendChild(tableRow);
+        }
+    });
+}
